1db8d00ee5c08be20f016f36e0bfbb3bd3df0359
kusi <lawcubegsm@gmail.com>
2022-09-07 15:14:45 +0000

    Add list responsiveness

--- src/module/generate_list.js
+++ src/module/generate_list.js
@@ -5,19 +5,19 @@ const generateList = (array) => {
   ul.setAttribute('class', 'food-list');
 
   for (let i = 0; i < array.length; i += 1) {
-    ul.innerHTML += `<li class="food-list-items" id = 'li-${array[i]['strCategory']}'>
-    <div class="list-header"><img src="${array[i]['strCategoryThumb']}" alt="food">img here</div>
+    ul.innerHTML += `<li class="food-list-items" id = 'li-${array[i]['idCategory']}'>
+    <div class="list-header"><img class="food-img" src="${array[i]['strCategoryThumb']}" alt="food"></div>
     <div class="list-body">
      <div class="list-names-likes">
       <p class="food-name">${array[i]['strCategory']}</p>
       <div class="like">
-       <img id="like-img" src="" alt="like">
+      <button type='button' id='like-img'><i class="fa-solid fa-heart"></i></button>
        <p><span>1</span>likes</p>
       </div>
      </div>
      <div class="list-buttons">
-      <button type="button" id = 'com-${array[i]['strCategory']}'>Comments</button>
-      <button type="button" id = 'res-${array[i]['strCategory']}' >Reservation</button>
+      <button type="button" id = 'com-${array[i]['idCategory']}'>Comments</button>
+      <button type="button" id = 'res-${array[i]['idCategory']}' >Reservation</button>
      </div>
     </div>
    </li>`;
